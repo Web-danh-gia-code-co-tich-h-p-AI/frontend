c9ecb92ad45227ec6985150fc3f7969a410c013c
To Quang Duc <toquangduc2004@gmail.com>
2024-05-09 01:10:40 +0700

    sửa lỗi vặt

--- src/components/blockfeatures/BlockUpload.jsx
+++ src/components/blockfeatures/BlockUpload.jsx
@@ -58,7 +58,7 @@ const BlockUpload = ({ generateContent }) => {
           xmlns="http://www.w3.org/2000/svg"
           viewBox="0 0 16 16"
           fill="currentColor"
-          className="w-6 mr-1 h-"
+          className="w-6 mr-1"
         >
           <path
             fillRule="evenodd"
