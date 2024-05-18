3fe3882b885331198b2a66569cf0726ab4427143
To Quang Duc <toquangduc2004@gmail.com>
2024-05-18 22:14:28 +0700

    thêm nút reload cho chart

--- src/components/chart/StatisticsScore.jsx
+++ src/components/chart/StatisticsScore.jsx
@@ -65,9 +65,23 @@ const StatisticsScore = () => {
           onClick={() => setReload(!reload)}
           className={`${
             loading ? "opacity-50 cursor-not-allowed" : ""
-          } p-2 mr-3 text-base font-normal text-white bg-blue-500 rounded-lg hover:bg-blue-600`}
+          } p-2 mr-3 text-base font-normal text-white bg-blue-500 rounded-lg hover:bg-blue-600 flex`}
           disabled={loading}
         >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className={`${loading ? "animate-spin" : ""} w-6 h-6 mr-2`}
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0 3.181 3.183a8.25 8.25 0 0 0 13.803-3.7M4.031 9.865a8.25 8.25 0 0 1 13.803-3.7l3.181 3.182m0-4.991v4.99"
+            />
+          </svg>
           {loading ? "Reloading..." : "Reload"}
         </button>
       </h1>
