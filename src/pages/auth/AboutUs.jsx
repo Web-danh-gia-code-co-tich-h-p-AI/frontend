87ab2ce9715f939a735b1649e0c4828d7e62d547
To Quang Duc <toquangduc2004@gmail.com>
2024-05-21 02:43:55 +0700

    réo=pnsive

--- src/pages/auth/AboutUs.jsx
+++ src/pages/auth/AboutUs.jsx
@@ -1,18 +1,20 @@
 const containerClasses =
   "min-h-screen flex flex-col items-center justify-center bg-white text-zinc-800 dark:bg-zinc-900 dark:text-white";
-const titleClasses = "text-6xl font-bold mb-4";
-const subTitleClasses = "text-4xl mb-2";
-const textClasses = "text-lg";
+const titleClasses = "laptop:text-6xl text-3xl font-bold mb-4 text-center";
+const subTitleClasses = "text-4xl mb-2 text-center";
+const textClasses = "text-lg text-center";
 
 const AboutUs = () => {
   return (
     <div className={containerClasses}>
-      <h1 className={titleClasses}>Đây là trang thông tin về chúng tôi</h1>
-      <p className={subTitleClasses}>Nhóm 6 lớp 22IT1 Trường Đại học CMC</p>
-      <p className={textClasses}>
-        Các thành viên trong nhóm đã tích cực học tập và thực hành để hoàn thành
-        bài dự án
-      </p>
+      <div className="w-2/3 laptop:w-3/4">
+        <h1 className={titleClasses}>Đây là trang thông tin về chúng tôi</h1>
+        <p className={subTitleClasses}>Nhóm 6 lớp 22IT1 Trường Đại học CMC</p>
+        <p className={textClasses}>
+          Các thành viên trong nhóm đã tích cực học tập và thực hành để hoàn thành
+          bài dự án
+        </p>
+      </div>
     </div>
   );
 };
